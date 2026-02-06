97efff5afa43d726bc3478b70cf02708c2895b44
ramana <ramanamuniyan26@gmail.com>
2026-02-06 22:30:01 +0530

    created child and executed cmd in child print the status of child

---
+++ Ass-7/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argv,char *argv[])
+{
+
+	if(argc < 2)
+	{
+	
+	perror("Usage %s args\n",argv[0]); // error message
+	
+	return 1;
+	}
+	
+	int status;
+	int ret=fork();
+	
+	if(ret > 0)
+	{
+	//parent 
+	
+	wait(&status);
+	if(WIFEXITED(status))
+	{
+	
+	printf("The child status is %d\n",WEXITSTATUS(status));
+	
+	}
+	
+	
+	}
+	
+	else if(ret ==0)
+	
+	{
+	 
+	 // child process 
+	 
+	 printf("The child process id %d\n",getpid()); //printing the child process
+	execvp(argv[1],argv[1]+1);//execute the cmd inside the child process
+	
+	
+	
+	}
+	
+
+}
+
