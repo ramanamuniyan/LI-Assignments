b03d1f52c1bf344f6873f834aa53c144b695e4a4
ramana <ramanamuniyan26@gmail.com>
2026-02-09 22:40:14 +0530

    ass-5 my own code

--- Ass-5/displaystate.c
+++ Ass-5/displaystate.c
@@ -1,88 +1,117 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include<sys/wait.h>
 #include <unistd.h>
-#include <string.h>
-#include <fcntl.h>
 
-void print(int fd);
-
+void print(int fd_1);
 int main()
 {
-    int ret = fork();
 
-    if (ret > 0) {
-        // parent
-        sleep(50);
-        printf("child is zombie\n");
+	int ret= fork();
+	
+	if(ret>0)
+	{
+	
+	//parent
+	
+	sleep(50);
+	printf("child is zombie\n ");
+int ret2=fork();
+	
+	if(ret2>0)
+	{
+	//parent 
+	
+	
+	}
+	else if(ret == 0)
+	{
+	//chid2 to check the parent is completed;
+	
+	//print the 
+	
+	int pid1=getppid();
+	
+	sleep(2);
+int pid2=getppid();
+	
+	if(pid1 != pid2)
+	{
+	// orphan
+	
+	char ps[16];
+	
+	snprintf(ps,sizeof(ps),"%d",getpid);
+	char file[20]="/proc/";
+	
+	
+	strcat(file,ps);
+	
+	strcat(file,"/status");
+	
+	
+	int fd_1=open(file,O_RDONLY);
+	
+	print(fd_1);
+	
 
-        int ret2 = fork();
+	}
+	
+	}
+	
+	
+	}
+	
+	else if(ret==0)
+	{
+	
+	//child 
+	
+	
+	int rets=getppid();
+	printf("The parent pid is -> %d\n",rets);
+	//printf("child is running\n");
+	
+	char ps[16];
+	
+	snprintf(ps,sizeof(ps),"%d",getpid);
+	char file[20]="/proc/";
+	
 
-        if (ret2 > 0) {
-            // parent exits
-            return 0;
-        }
-        else if (ret2 == 0) {
-            // child2 (orphan checker)
-
-            int pid1 = getppid();
-            sleep(2);
-            int pid2 = getppid();
-
-            if (pid1 != pid2) {
-                char ps[16];
-                char file[64] = "/proc/";
-
-                snprintf(ps, sizeof(ps), "%d", getpid());
-                strcat(file, ps);
-                strcat(file, "/status");
-
-                int fd_1 = open(file, O_RDONLY);
-                if (fd_1 >= 0) {
-                    print(fd_1);
-                    close(fd_1);
-                }
-            }
-        }
-    }
-    else if (ret == 0) {
-        // child
-        printf("The parent pid is -> %d\n", getppid());
-
-        char ps[16];
-        char file[64] = "/proc/";
-
-        snprintf(ps, sizeof(ps), "%d", getpid());
-        strcat(file, ps);
-        strcat(file, "/status");
-
-        int fd_1 = open(file, O_RDONLY);
-        if (fd_1 >= 0) {
-            print(fd_1);
-            close(fd_1);
-        }
-
-        sleep(25);
-    }
-
-    return 0;
+	strcat(file,ps);
+	
+	strcat(file,"/status");
+	
+	
+	int fd_1=open(file,O_RDONLY);
+	
+	print(fd_1);
+	sleep(25);
+	
+	
+	
+	}
 }
 
 void print(int fd)
 {
-    char buf[256];
-    int bytes, lines = 0;
+	char buf[256];
+	int bytes, lines = 0;
 
-    while ((bytes = read(fd, buf, sizeof(buf))) > 0) {
-        for (int i = 0; i < bytes; i++) {
-            write(STDOUT_FILENO, &buf[i], 1);
+	while ((bytes = read(fd, buf, sizeof(buf))) > 0) {
+    	for (int i = 0; i < bytes; i++) 
+    	{
+        write(1	, &buf[i], 1);
 
-            if (buf[i] == '\n') {
-                lines++;
-                if (lines == 3)
-                    return;   // print only 3 lines
-            }
+        if (buf[i] == '\n') 
+        {
+            lines++;
+            if (lines == 3)
+                return;   // stop after 3 lines
         }
     }
+}
+	
+
 }
 
